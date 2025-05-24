a589452b142ab8647e4d83051c6b9e2e9406721c
Aditya <adityabohra619@gmail.com>
2025-05-24 11:58:12 +0530

    fixing for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 // next.config.js or next.config.ts
-const isProd = process.env.NODE_ENV === 'production';
-const isExport = process.env.NEXT_EXPORT === 'true';
+// const isProd = process.env.NODE_ENV === 'production';
+// const isExport = process.env.NEXT_EXPORT === 'true';
 
 const nextConfig = {
   output: 'export',
-  basePath: isProd && isExport ? '/adityabohra007.github.io' : '',
-  assetPrefix: isProd && isExport ? '/adityabohra007.github.io/' : '',
+  // basePath: isProd && isExport ? '/adityabohra007.github.io' : '',
+  // assetPrefix: isProd && isExport ? '/adityabohra007.github.io/' : '',
 };
 
 export default nextConfig;
